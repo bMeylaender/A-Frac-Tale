6663c1f9b976eb708fad495e56adadb1d5c3e000
bMeylaender <meylaenderbaptiste@gmail.com>
2025-05-15 16:57:52 +0200

    ajout animation en scrollant

--- assets/javascript/script_accueil.js
+++ assets/javascript/script_accueil.js
@@ -1,14 +1,39 @@
-const observer = new IntersectionObserver(
-    entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('in-view');
-        }
-      });
-    },
-    { threshold: 0.1 }
-);
+let scroll = 0;
 
-document.querySelectorAll('.fade-in-up').forEach(el => {
-    observer.observe(el);
+document.addEventListener("wheel", function (e) {
+  const hero = document.querySelector("#hero");
+  const wave = document.querySelector(".wave");
+  const header = document.querySelector("#header");
+  const outils = document.querySelector("#outils");
+  const fadeIn = document.querySelectorAll(".fade-in-up");
+
+  scroll += e.deltaY;
+  if (scroll < 0) {
+    scroll = 0;
+  }
+  if (scroll > 1200) {
+    scroll = 1200;
+  }
+  console.log(scroll);
+
+  if (scroll > 0) {
+    hero.classList.add("scrolled");
+    wave.classList.add("scrolled");
+    setTimeout(() => {
+      header.classList.add("active");
+    }, 200);
+    setTimeout(() => {
+      fadeIn.forEach((element) => element.classList.add("in-view"));
+    }, 600);
+    if (scroll > 600) {
+      outils.classList.add("scrolled");
+    } else {
+      outils.classList.remove("scrolled");
+    }
+  } else {
+    hero.classList.remove("scrolled");
+    wave.classList.remove("scrolled");
+    header.classList.remove("active");
+    fadeIn.forEach((element) => element.classList.remove("in-view"));
+  }
 });
